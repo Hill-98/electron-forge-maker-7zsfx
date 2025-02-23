75d93d0d154329accdcffe2a6a701d35e2e72e17
Zhong Lufan <lufanzhong@gmail.com>
2025-02-23 19:33:30 +0800

    chore(demo): add icon

--- demo/forge.config.js
+++ demo/forge.config.js
@@ -2,6 +2,9 @@ const SevenZSFXMaker = require('../')
 
 /** @type {import('@electron-forge/shared-types').ForgeConfig} */
 const config = {
+  packagerConfig: {
+    icon: __dirname.concat('/icon'),
+  },
   makers: [new SevenZSFXMaker()],
 }
 
